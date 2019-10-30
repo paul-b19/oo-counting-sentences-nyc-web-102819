bb34144e951fbfecda9797df8bcc0dc91a822b2a
Pavel Beletsky <pavel.beletsky@live.com>
2019-10-30 07:04:08 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    replacement = {"!" => ".", "?" => "."}
+    new_self = self.split('').map{|i| replacement[i] || i}.join
+    new_self.split('.').reject{|c| c.empty?}.count
   end
 end
